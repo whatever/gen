6f2b9109541327b650726db7145adcc66db580d6
Matt 3 <not.mattowen@gmail.com>
2021-03-14 13:28:27 -0400

    :wolf: Link to image from canvas + generate bytes from canvas

--- src/zine.js
+++ src/zine.js
@@ -72,6 +72,13 @@ export class Zine {
         return this.el.toDataURL('image/png');
     }
 
+    getImageData() {
+        let gl = this.ctx;
+        let pixels = new Uint8Array(gl.drawingBufferWidth * gl.drawingBufferHeight * 4);
+        gl.readPixels(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+        console.log(pixels);
+    }
+
     eventHandlers() {
         return {};
     }
