c0a3417b99a17b1c7caf44b4e898786865820563
Matt 3 <not.mattowen@gmail.com>
2021-03-28 00:52:27 -0400

    :eyes: generate lots of eyes on a page

--- src/gen/eyes.js
+++ src/gen/eyes.js
@@ -7,6 +7,38 @@ export class Eyes {
     }
 
     setup() {
+        this.ctx.drawEye = (x, y, size) => {
+
+            let x_offset = size/4.0;
+            let y_offset = size/1.5;
+
+            // Whole eye
+            this.ctx.beginPath();
+            this.ctx.moveTo(x-size, y);
+            this.ctx.bezierCurveTo(
+                x-x_offset, y-y_offset,
+                x+x_offset, y-y_offset,
+                x+size, y,
+            );
+            this.ctx.bezierCurveTo(
+                x+x_offset, y+y_offset,
+                x-x_offset, y+y_offset,
+                x-size, y,
+            );
+            this.ctx.fillStyle = "white";
+            this.ctx.fill();
+            this.ctx.stroke();
+            this.ctx.closePath();
+            
+            // Iris
+            this.ctx.beginPath();
+            let radius = size/2.0-1.0;
+            this.ctx.moveTo(x+radius, y);
+            this.ctx.arc(x, y, radius, 0, 2*Math.PI);
+            this.ctx.fillStyle = "black";
+            this.ctx.fill();
+            this.ctx.closePath();
+        };
     }
 
     update() {
@@ -18,9 +50,17 @@ export class Eyes {
         this.ctx.fillRect(0, 0, this.el.width, this.el.height);
 
         this.ctx.save();
-            this.ctx.beginPath();
-            this.ctx.rect(10, 10, 100, 100);
-            this.ctx.stroke();
+            // this.ctx.beginPath();
+            // this.ctx.rect(10, 10, 100, 100);
+            // this.ctx.stroke();
+
+            for (let i=0; i < 100; i++) {
+                let x = Math.random()*this.el.width;
+                let y = Math.random()*this.el.height;
+                let s = Math.random()*100+25;
+                this.ctx.drawEye(x, y, s);
+            }
+
         this.ctx.restore();
     }
 }
